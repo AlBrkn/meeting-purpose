73b11df16c179943a05bac67664486a4518c36f5
aleksey_brehin <aleksey_brehin@epam.com>
2022-08-28 02:16:54 +0300

    initial commit

---
+++ src/main/java/ru/brekhin/meetingpurpose/MeetingPurposeApplication.java
@@ -0,0 +1,13 @@
+package ru.brekhin.meetingpurpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingPurposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingPurposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/brekhin/meetingpurpose/MeetingPurposeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.brekhin.meetingpurpose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingPurposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
